e2cb4c6648fd5d7c6853dfd614097c763af04750
MiaCurrid <currm057.202@uclacademy.org>
2016-03-13 11:33:07 +0000

    1st aid moved

--- doodle.py
+++ doodle.py
@@ -58,7 +58,7 @@ def to_camera_space(rect):
 
 ## items!!!
 items = pygame.sprite.OrderedUpdates()
-items.add(create_item("first_aid", 400,1550)) 
+items.add(create_item("first_aid", 400,1450)) 
 
 platforms = []
 platform_colour = (200, 140, 80)
