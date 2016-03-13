24f02f6b56ff0760ab0a3cad3d7ecebc147edf56
reshma15 <begur561.202@uclacademy.org>
2016-03-13 13:32:42 +0000

    heightened the new platform

--- doodle.py
+++ doodle.py
@@ -71,7 +71,7 @@ platforms.append(create_platform(200, 1200+100))
 
 platforms.append(create_platform(600, 1000+100))
 platforms.append(create_platform(200, 890))
-platforms.append(create_platform(100, 700))
+platforms.append(create_platform(100, 600))
 
 platforms.append(bottom)
 
